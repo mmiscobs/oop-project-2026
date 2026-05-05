da2707410fc87fb700fa060ad3167592de6ee8f0
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2026-05-06 00:18:48 +0400

    feat: improve game speed reliability

--- src/simulation/GameSpeed.java
+++ src/simulation/GameSpeed.java
@@ -2,8 +2,8 @@ package simulation;
 
 public enum GameSpeed {
     Stopped(Integer.MAX_VALUE),
-    Slow(1000),
-    Normal(500),
+    Slow(1600),
+    Normal(600),
     Fast(200);
 
     public final int msBetweenTicks;
